0f0426d130ee68caf4d0151ef9ce233e9db62345
DAhme313 <da85150@gmail.com>
2021-03-31 18:34:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -50,23 +50,17 @@ Try to describe what a variable is, in your own words.
 Write the code to execute an addition (a sum) of the numbers 12 and 20.
 */
 
-let oneVariable = "12"
-let secondVariable = "20"
-console.log("oneVariable + 20")
-
-
 /* WRITE YOUR CODE HERE */
+
+let result = 12 * 20
 
 /* EXERCISE 4
 Create a variable named x containing the number 12.
 */
 
 /* WRITE YOUR CODE HERE */
-let myVariable;
-myVariable = "x";
-alert(x);
 
-alert(x); 
+let x = 12
 
 /* EXERCISE 5
 Create a variable called name containing the string John Doe.
@@ -74,15 +68,14 @@ Create a variable called name containing the string John Doe.
 
 
 /* WRITE YOUR CODE HERE */
-let oneVariable;
-oneVariable = "John Doe"
-(alert = "John Doe")
+let name = "John Doe"
 
 /* EXERCISE 6
 Execute a subtraction between the number 12 and the variable x, which is storing the value 12.
 */
 
 /* WRITE YOUR CODE HERE */
+let ex6result = 12 * 2
 
 /* EXERCISE 7
 Create two variables: name1 and name2. name1 is equal to john, name2 is equal to John.
@@ -91,18 +84,37 @@ Verify then, that name1 and name2 are equals if both lowercase (without changing
 */
 
 /* WRITE YOUR CODE HERE */
+let name = "john"
+let name2 = "John"
+console.log(name1 === name2)
 
 /* EXERCISE 8
 Create a variable named x (its value must be less than 10). Write the code to print the literal value of x (ex.: if x is 1 print "one", if 5 print "five" etc.).
 */
 
 /* WRITE YOUR CODE HERE */
+x = 6
+if (x === 1)
+    console.long("one")
+else if (x === 2)
+    console.log("two")
+    
+//another method 
+
+switch (x){
+    case 1: console.log("one"); break;
+    case 1: console.log("two"); break;
+defualt: console.log("This works for numbers between 1 to 9 (included")
+}
 
 /* EXERCISE 9
 [Extra] Insert a value in a variable based on the resut of a ternary if (topic not covered during lesson, try to search it by yourself, tomorrow morning we'll discuss it together ;) )
 */
 
 /* WRITE YOUR CODE HERE */
+let TernaryExample = x > 10 ? true : false
+
+
 
 /* WHEN YOU ARE FINISHED
 Upload the .js file on Eduflow before 5PM CET. In the next days we'll also learn how to use GIT!
